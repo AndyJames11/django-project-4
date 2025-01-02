d78532c7be12b13f1f431a551739c0bafb6b0b39
AndyJames11 <andrewjamesh11@googlemail.com>
2025-01-02 14:58:27 +0000

    remove whitenoise from settings file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_6kvh#i@s#hw%qacn9d%o9t=-vhn-u=v*v@zv=&j@2f99vf4b#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ah-django-project-4-9a769b59e2b7.herokuapp.com']
+ALLOWED_HOSTS = ['ah-django-project-4-9a769b59e2b7.herokuapp.com', 'ah-django-project-4.herokuapp.com']
 
 
 # Application definition
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,8 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
